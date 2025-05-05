bdb1a6315556a25ed381b3aeddd07a0fc26c7de6
Harsha-Srinatth <harhsasrinatth5599@gmail.com>
2025-05-05 22:55:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const path = require('path');
 const app = express();
 app.use('/uploads', 
 express.static(path.join(__dirname,'/uploads')));
-app.use(cors());
+app.use(cors({
+    origin:'https://react-first-p1.vercel.app'
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 dotEnv.config();
