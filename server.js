0a4e784312941dd518277419237ccd91870d2bd4
Harsha-Srinatth <harhsasrinatth5599@gmail.com>
2025-05-05 22:08:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URL,{
 
 app.use('/api-auth', authRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT ||
 app.listen(PORT, ()=>{
     console.log(`Server started and running at ${PORT}`)
 })
