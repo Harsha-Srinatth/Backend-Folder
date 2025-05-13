79b9789c957a94dda8f69844f951290166b51a88
Harsha-Srinatth <harhsasrinatth5599@gmail.com>
2025-05-13 10:23:25 +0530

    ds

--- controllers/getCUDetails.js
+++ controllers/getCUDetails.js
@@ -31,7 +31,7 @@ exports.getCUDetails = async (req, res) => {
     const response = {
       ...userDetails,
       image: {
-        imageUrl: userImage
+        userImage
       },
       followingCount,
       followersCount
