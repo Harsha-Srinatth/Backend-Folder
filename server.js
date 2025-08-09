c59b22717091478d54114a1dfde25ff5b86c971c
Harsha-Srinatth <harhsasrinatth5599@gmail.com>
2025-08-09 16:18:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/uploads',
     express.static(uploadDir));
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://my-frontend-kohl.vercel.app',
     credentials: true,
     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
